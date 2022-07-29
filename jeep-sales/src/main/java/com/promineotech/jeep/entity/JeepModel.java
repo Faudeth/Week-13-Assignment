d3e4e1e97adfc845fc027f5847add96274b78e2b
Jacob Pool <58780726+Faudeth@users.noreply.github.com>
2022-07-29 13:07:42 -0600

    Add files via upload

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author Jacob
+ *
+ */
+public enum JeepModel {
+  
+    WRANGLER, GRAND_CHEROKEE, CHEROKEE, COMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+
+}
